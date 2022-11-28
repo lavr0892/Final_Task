76a85e3e4af24ead97bff18880dfb682b43f0328
Jane <lavr0892@yandex.ru>
2022-11-28 12:01:59 +0300

    Task is ready

---
+++ task/Program.cs
@@ -0,0 +1,34 @@
+﻿void ReleaseMatrix(string[] matrix, string[] matrix2)
+{
+    int count = 0;
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        if (matrix[i].Length <= 3)
+        {
+            matrix2[count] = matrix[i];
+            count++;          
+        }
+        Console.Write(matrix2[i]);
+
+    }
+}
+    void PrintMatrix(string[] matrix)
+    {
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            Console.Write($"{matrix[i] + " "}");
+
+        }
+    }
+
+    Console.Clear();
+    Console.WriteLine("Введите элементы массива через пробел: ");
+    string[] matrix = Console.ReadLine().Split(' ');
+    string[] matrix2 = new string[matrix.Length];
+
+    Console.WriteLine("Начальный массив: ");
+    PrintMatrix(matrix);
+    ReleaseMatrix(matrix, matrix2);
+    Console.WriteLine();
+    Console.WriteLine("Конечный массив: ");
+    PrintMatrix(matrix2);
